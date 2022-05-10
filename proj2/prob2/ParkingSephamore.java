89e599092baf21575a6e871e0fbf3fed1f2376af
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-05-10 19:38:54 +0900

    Add problem 2

---
+++ proj2/prob2/ParkingSephamore.java
@@ -0,0 +1,89 @@
+import java.util.concurrent.Semaphore;
+
+class ParkingGarage {
+  private int places;
+  Semaphore sem;
+
+  public ParkingGarage(int places) {
+    if (places < 0)
+      places = 0;
+    this.places = places;
+    this.sem = new Semaphore(this.places);
+  }
+
+  public void enter() {
+    try {
+      this.sem.acquire();
+      this.places--;
+      this.sem.release();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void leave() {
+    try {
+      this.sem.acquire();
+      this.places++;
+      this.sem.release();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
+
+class Car extends Thread {
+  private ParkingGarage parkingGarage;
+
+  public Car(String name, ParkingGarage p) {
+    super(name);
+    this.parkingGarage = p;
+    start();
+  }
+
+  private void tryingEnter() {
+    System.out.println(getName() + ": trying to enter");
+  }
+
+  private void justEntered() {
+    System.out.println(getName() + ": just entered");
+
+  }
+
+  private void aboutToLeave() {
+    System.out.println(getName() + ":                                     about to leave");
+  }
+
+  private void Left() {
+    System.out.println(getName() + ":                                     have been left");
+  }
+
+  public void run() {
+    while (true) {
+      try {
+        sleep((int) (Math.random() * 10000));
+      } catch (InterruptedException e) {
+      }
+      tryingEnter();
+      parkingGarage.enter();
+      justEntered();
+      try {
+        sleep((int) (Math.random() * 20000));
+      } catch (InterruptedException e) {
+      }
+      aboutToLeave();
+      parkingGarage.leave();
+      Left();
+
+    }
+  }
+}
+
+public class ParkingSephamore {
+  public static void main(String[] args) {
+    ParkingGarage parkingGarage = new ParkingGarage(7);
+    for (int i = 1; i <= 10; i++) {
+      new Car("Car " + i, parkingGarage);
+    }
+  }
+}
